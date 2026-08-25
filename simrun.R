b58411fba2e040c9a89a726eadec8857928e2977
Ian White <35139755+ianrwhite@users.noreply.github.com>
2026-08-25 14:08:15 +0100

    Set up as run=1/2 and correct nonnull for run==2

--- simrun.R
+++ simrun.R
@@ -12,24 +12,29 @@ crossover <- 0.08
 common_shape <- 1.1
 acc_period <- 36
 time_censor <- 48
+run <- 2 # 1: 7 classes incl. HF; 2: 6 classes excl. HF
 
-# parameters with HF patients (7 subgroups)
-# prevalence_set <- c(0.25, 0.04, 0.06, 0.27, 0.24, 0.09, 0.05)
-# cont_cuminc_5y <- c(.623, .490, .398, .609, .392, .720, .689) # Weiqi's 5-year cum inc for PCI group
-
-# parameters without HF patients (6 subgroups)
-# figures from Weiqi 24/8/2026
-nobs <- c(21878, 6477, 24506, 23179, 6153, 4323)
-nobspci <- c(8002, 2138, 9336, 16331, 1523, 875)
-neventpci <- c(4857, 850, 5605, 6350, 1017, 591) # all-cause mortality or CV hospitalisation to 5 years
-prevalence_set <- nobs / sum(nobs)
-cont_cuminc_5y <- neventpci / nobspci
-
+if(run==1) {
+  # parameters with HF patients (7 subgroups)
+  prevalence_set <- c(0.25, 0.04, 0.06, 0.27, 0.24, 0.09, 0.05)
+  cont_cuminc_5y <- c(.623, .490, .398, .609, .392, .720, .689) # Weiqi's 5-year cum inc for PCI group
+}
+if(run==2){
+  # parameters without HF patients (6 subgroups)
+  # figures from Weiqi 24/8/2026
+  nobs <- c(21878, 6477, 24506, 23179, 6153, 4323)
+  nobspci <- c(8002, 2138, 9336, 16331, 1523, 875)
+  neventpci <- c(4857, 850, 5605, 6350, 1017, 591) # all-cause mortality or CV hospitalisation to 5 years
+  prevalence_set <- nobs / sum(nobs)
+  cont_cuminc_5y <- neventpci / nobspci
+}
+# derived parameters
+K <- length(prevalence_set)
 scale_con <- 60 * (-log(1-cont_cuminc_5y))^(-1/common_shape) # Weibull calculation
 
-set.seed(101)
-
-K <- length(prevalence_set)
+# simulation settings
+N_reps <- 5000
+# I'll set seed before each run, so that it can be run in parts
 
 
 sim_run <- function(nonnull, N_tot, N_reps) {
@@ -61,7 +66,7 @@ sim_run <- function(nonnull, N_tot, N_reps) {
     avepower <- sum(nonnull * powers) / sum(nonnull) # average power
     wtavepower <- sum(prevalence_set * nonnull * powers) /
       sum(nonnull * prevalence_set) # weighted average power
-    if (i==1) ressep <- cbind(t(powers), fwer, avepower, wtavepower)
+    if(i==1) ressep <- cbind(t(powers), fwer, avepower, wtavepower)
     else  resborrow <- cbind(t(powers), fwer, avepower, wtavepower)
   }
   # tidy up and output
@@ -74,45 +79,63 @@ sim_run <- function(nonnull, N_tot, N_reps) {
   return(res)
 }
 
-N_reps <- 1000
-
 ### GLOBAL NULL ###
-resgn <- sim_run(nonnull = rep(0, K), N_tot = 1500, N_reps = N_reps)
+set.seed(101)
+# resgn <- sim_run(nonnull = rep(0, K), N_tot = 1500, N_reps = N_reps)
 resgn
 
 ### GLOBAL ALTERNATIVE, VARY N ###
-resga1 <- sim_run(nonnull = rep(1, K), N_tot = 1500, N_reps = N_reps)
-resga1
-resga2 <- sim_run(nonnull = rep(1, K), N_tot = 2000, N_reps = N_reps)
-resga2
-resga3 <- sim_run(nonnull = rep(1, K), N_tot = 2500, N_reps = N_reps)
-resga3
-resga4 <- sim_run(nonnull = rep(1, K), N_tot = 3000, N_reps = N_reps)
-resga4
+for(i in 1:4) {
+  set.seed(101)
+  N_tot=1000+500*i # 1500 to 3000
+  thisresult = paste0("resga",i)
+  # assign(thisresult, sim_run(nonnull = rep(1, K), N_tot = N_tot, N_reps = N_reps))
+  print(get(thisresult))
+}
 
-N_tot <- 3000
-### PARTIAL ALTERNATIVE 1: ONLY SMALLEST SUBGROUP (2) IS NULL
-respa1 <- sim_run(nonnull = c(1, 0, 1, 1, 1, 1, 1), N_tot = N_tot, N_reps = N_reps)
-respa1
-### PARTIAL ALTERNATIVE 2: ONLY LARGEST SUBGROUP (4) IS NULL
-respa2 <- sim_run(nonnull = c(1, 1, 1, 0, 1, 1, 1), N_tot = N_tot, N_reps = N_reps)
-respa2
-### PARTIAL ALTERNATIVE 3: ONLY SMALLEST SUBGROUP (2) IS NON-NULL
-respa3 <- sim_run(nonnull = c(0, 1, 0, 0, 0, 0, 0), N_tot = N_tot, N_reps = N_reps)
-respa3
-### PARTIAL ALTERNATIVE 4: ONLY LARGEST SUBGROUP (4) IS NON-NULL
-respa4 <- sim_run(nonnull = c(0, 0, 0, 1, 0, 0, 0), N_tot = N_tot, N_reps = N_reps)
-respa4
-### PARTIAL ALTERNATIVE 5: THREE LARGEST SUBGROUPS (5,1,4) ARE NULL
-respa5 <- sim_run(nonnull = c(0, 1, 1, 0, 0, 1, 1), N_tot = N_tot, N_reps = N_reps)
-respa5
-### PARTIAL ALTERNATIVE 6: THREE LARGEST SUBGROUPS (5,1,4) ARE NON-NULL
-respa6 <- sim_run(nonnull = c(1, 0, 0, 1, 1, 0, 0), N_tot = N_tot, N_reps = N_reps)
-respa6
+### PARTIAL ALTERNATIVES, VARY NONNULL ###
+if(run==1) {
+  N_tot <- 3000
+  ### PARTIAL ALTERNATIVE 1: ONLY SMALLEST SUBGROUP (2) IS NULL
+  nonnull_pa1 <- c(1, 0, 1, 1, 1, 1, 1)
+  ### PARTIAL ALTERNATIVE 2: ONLY LARGEST SUBGROUP (4) IS NULL
+  nonnull_pa2 <- c(1, 1, 1, 0, 1, 1, 1)
+  ### PARTIAL ALTERNATIVE 3: ONLY SMALLEST SUBGROUP (2) IS NON-NULL
+  nonnull_pa3 <- c(0, 1, 0, 0, 0, 0, 0)
+  ### PARTIAL ALTERNATIVE 4: ONLY LARGEST SUBGROUP (4) IS NON-NULL
+  nonnull_pa4 <- c(0, 0, 0, 1, 0, 0, 0)
+  ### PARTIAL ALTERNATIVE 5: THREE LARGEST SUBGROUPS (5,1,4) ARE NULL
+  nonnull_pa5 <- c(0, 1, 1, 0, 0, 1, 1)
+  ### PARTIAL ALTERNATIVE 6: THREE LARGEST SUBGROUPS (5,1,4) ARE NON-NULL
+  nonnull_pa6 <- c(1, 0, 0, 1, 1, 0, 0)
+}
+if(run==2) {
+  N_tot <- 2500
+  ### PARTIAL ALTERNATIVE 1: ONLY SMALLEST SUBGROUP (6) IS NULL
+  nonnull_pa1 <- c(1, 1, 1, 1, 1, 0)
+  ### PARTIAL ALTERNATIVE 2: ONLY LARGEST SUBGROUP (3) IS NULL
+  nonnull_pa2 <- c(1, 1, 0, 1, 1, 1)
+  ### PARTIAL ALTERNATIVE 3: ONLY SMALLEST SUBGROUP (6) IS NON-NULL
+  nonnull_pa3 <- c(0, 0, 0, 0, 0, 1)
+  ### PARTIAL ALTERNATIVE 4: ONLY LARGEST SUBGROUP (3) IS NON-NULL
+  nonnull_pa4 <- c(0, 0, 1, 0, 0, 0)
+  ### PARTIAL ALTERNATIVE 5: THREE LARGEST SUBGROUPS (1,3,4) ARE NULL
+  nonnull_pa5 <- c(0, 1, 0, 0, 1, 1)
+  ### PARTIAL ALTERNATIVE 6: THREE LARGEST SUBGROUPS (1,3,4) ARE NON-NULL
+  nonnull_pa6 <- c(1, 0, 1, 1, 0, 0)
+}
+for(i in 1:6){
+  set.seed(101)
+  thisresult <- paste0("respa",i) 
+  thisnonnull <- paste0("nonnull_pa",i)
+  assign(thisresult, sim_run(get(thisnonnull), N_tot = N_tot, N_reps = N_reps))
+  print(get(thisresult))
+}
+
 ### SAVE AND END ###
-
 allresults <- rbind(resgn, resga1, resga2, resga3, resga4, respa1, respa2, respa3, respa4, respa5, respa6)
 library(writexl)
-write_xlsx(as.data.frame(allresults), "simrun_results_v2.xlsx")
-save.image(file = "simrun.Rdata")
+outfile <- paste0("simrun_results_run", run)
+write_xlsx(as.data.frame(allresults), paste0(outfile,".xlsx"))
+save.image(file = paste0(outfile,".Rdata"))
 # 5000 reps took about 12h
